1a266a71f8e58f73dfee47f20bcf6fa858f97640
Wilson Corn <wilson.corn@hotmail.com>
2025-04-23 10:33:01 -0700

    Update urun.js

--- urun.js
+++ urun.js
@@ -3,7 +3,7 @@ document.addEventListener("keydown", function (e) {
 	if (e.key == "!" && e.ctrlKey) {
 		var t = window.open("", "_blank", "width=500,height=300");
 		var e = t.document.createElement("iframe");
-		(e.src = "//ustuff.github.io/uRun/popup.html"),
+		(e.src = "https://raw.githubusercontent.com/wicorn29/urunte/refs/heads/main/popup.html"),
 			(e.style.cssText = "width:100%; height:100%; border:none;"),
 			t.document.body.appendChild(e),
 			t.document.title = "uRun",
